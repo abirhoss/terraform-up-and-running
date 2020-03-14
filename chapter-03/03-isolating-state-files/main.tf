6dee3a11dd043d01ab2a8b1b61cf4b569ec6421f
Abir Hossain <abir.hossain@sbs.com.au>
2020-03-14 12:29:55 +1100

    chapter-03-03 Isolating state files

---
+++ chapter-03/03-isolating-state-files/main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform-up-and-running-state-abirhoss"
+    key            = "workspaces-example/terraform.tfstate"
+    region         = "us-east-2"
+
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+}
+
+# # Ternary syntax to conditionally set instance_type depending on current workspace
+# resource 'aws_instance' 'example' {
+#   ami           = 'ami-0c55b159cbfafe1f0'
+#   instance_type = terraform.workspace == 'default' ? 't2.medium' : 't2.micro'
+# }
