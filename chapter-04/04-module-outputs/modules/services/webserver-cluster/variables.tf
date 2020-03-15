b9e0a6fa7c390e167ef7f05afd7fa38ed58a4f30
Abir Hossain <abir.hossain@sbs.com.au>
2020-03-15 14:13:18 +1100

    chapter-04 Module Gotchas

--- chapter-04/04-module-outputs/modules/services/webserver-cluster/variables.tf
+++ chapter-04/04-module-outputs/modules/services/webserver-cluster/variables.tf
@@ -10,12 +10,12 @@ variable "cluster_name" {
 }
 
 variable "db_remote_state_bucket" {
-  description = "The name of the S3 bucket for the database's remote state"
+  description = "The name of the S3 bucket for the databases remote state"
   type        = string
 }
 
 variable "db_remote_state_key" {
-  description = "The path for the database's remote state in S3"
+  description = "The path for the databases remote state in S3"
   type        = string
 }
 
--- chapter-04/04-module-outputs/stage/services/webserver-cluster/main.tf
+++ chapter-04/04-module-outputs/stage/services/webserver-cluster/main.tf
@@ -9,7 +9,7 @@ module "webserver_cluster" {
   db_remote_state_bucket = "terraform-up-and-running-state-abirhoss"
   db_remote_state_key    = "stage/data-stores/mysql/terraform.tfstate"
 
-  instance_type = 't2.micro'
+  instance_type = "t2.micro"
   min_size      = 2
   max_size      = 2
 }
